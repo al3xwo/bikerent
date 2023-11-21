afec051994153a3e8ebcb9617489f3575f08c950
David Julian <david_a80@hotmail.com>
2023-11-21 18:20:27 +0100

    working on routes

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,2 +1,54 @@
 class BikesController < ApplicationController
+  before_action :set_bike, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @bikes = Bike.all
+  end
+
+  def show
+    # Assumes set_bike method sets @bike instance variable
+  end
+
+  def new
+    @bike = Bike.new
+  end
+
+  def create
+    @bike = Bike.new(bike_params)
+
+    if @bike.save
+      redirect_to @bike, notice: 'Bike was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    # Assumes set_bike method sets @bike instance variable
+  end
+
+  def update
+    # Assumes set_bike method sets @bike instance variable
+
+    if @bike.update(bike_params)
+      redirect_to @bike, notice: 'Bike was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @bike.destroy
+    redirect_to bikes_url, notice: 'Bike was successfully destroyed.'
+  end
+
+  private
+
+  def set_bike
+    @bike = Bike.find(params[:id])
+  end
+
+  def bike_params
+    params.require(:bike).permit(:attribute1, :attribute2, :attribute3)
+  end
 end
