4c0b4fdbd0b531921f2f367ed39958b3a81711ec
Katerina Mastrogianni <katerinamastrog@gmail.com>
2023-11-21 16:54:25 +0100

    made validations in Bike model and Booking

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,3 +1,9 @@
 class Bike < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :description, presence: true
+  validates :category, presence: true
+  validates :location, presence: true
 end
