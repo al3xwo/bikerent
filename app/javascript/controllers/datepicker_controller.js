6247318cc69290e45e8d75374a0e48a7efb7803a
Katerina Mastrogianni <katerinamastrog@gmail.com>
2023-11-22 16:27:59 +0100

    working on the price incrementation

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -2,7 +2,17 @@ import { Controller } from "@hotwired/stimulus"
 import flatpickr from "flatpickr"; // You need to import this to use new flatpickr()
 // Connects to data-controller="datepicker"
 export default class extends Controller {
+  static values = { price: Number }
+
   connect() {
-  flatpickr(this.element)
+    console.log('Hello from insanity');
+    console.log('this.priceValue', this.priceValue);
+    const price = this.priceValue;
+    flatpickr(this.element, {
+      mode: "range",
+      onChange: function (selectedDates, _dateStr, _instance) {
+        console.dir(((selectedDates[1] - selectedDates[0]) / 86400000) * price);
+      }
+    })
   }
 }
