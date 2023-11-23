15a1e3a036ce69c7a48cc8131e7677eba0a21014
Katerina Mastrogianni <katerinamastrog@gmail.com>
2023-11-23 12:20:07 +0100

    able to book bike

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -4,15 +4,17 @@ import flatpickr from "flatpickr"; // You need to import this to use new flatpic
 export default class extends Controller {
   static values = { price: Number }
 
+  static targets = ['dateField']
+
   connect() {
     console.log('Hello from insanity');
     console.log('this.priceValue', this.priceValue);
     const price = this.priceValue;
-    flatpickr(this.element, {
+    flatpickr(this.dateFieldTarget, {
       mode: "range",
-      onChange: function (selectedDates, _dateStr, _instance) {
-        console.dir(((selectedDates[1] - selectedDates[0]) / 86400000) * price);
-      }
+      // onChange: function (selectedDates, _dateStr, _instance) {
+      //   console.dir(((selectedDates[1] - selectedDates[0]) / 86400000) * price);
+      // }
     })
   }
 }
