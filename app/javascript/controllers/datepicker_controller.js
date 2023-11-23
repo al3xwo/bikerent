511d47060feb981fc88ba12f6f158d83cf3aba7f
Katerina Mastrogianni <katerinamastrog@gmail.com>
2023-11-23 13:05:12 +0100

    Updated seed prices

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -3,18 +3,18 @@ import flatpickr from "flatpickr"; // You need to import this to use new flatpic
 // Connects to data-controller="datepicker"
 export default class extends Controller {
   static values = { price: Number }
-
-  static targets = ['dateField']
+  static targets = ['dateField', 'priceDisplay']
 
   connect() {
-    console.log('Hello from insanity');
-    console.log('this.priceValue', this.priceValue);
-    const price = this.priceValue;
+    const handleChange = (selectedDates, _dateStr, _instance) => {
+      const totalPrice = ((selectedDates[1] - selectedDates[0]) / 86400000) * this.priceValue;
+      if (!isNaN(totalPrice)) {
+        this.priceDisplayTarget.textContent = totalPrice;
+      }
+    }
     flatpickr(this.dateFieldTarget, {
       mode: "range",
-      // onChange: function (selectedDates, _dateStr, _instance) {
-      //   console.dir(((selectedDates[1] - selectedDates[0]) / 86400000) * price);
-      // }
+      onChange: handleChange,
     })
   }
 }
